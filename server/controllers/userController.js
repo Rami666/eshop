81ad8bb917008b60f24316e7475a77b5598635d7
Rami B <ramibasheer9@gmail.com>
2022-12-16 22:31:54 +0100

    apply backed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,3 +12,37 @@ module.exports.register = async (req, res) => {
     res.send({ success: false, error: error.message });
   }
 };
+
+
+module.exports.login = async (req, res) => {
+  try {
+
+    console.log("hello from register", req.body);
+
+    const user = await User.findOne(req.body).select("-password -__v");
+
+
+    res.send({ success: true,user });
+  } catch (error) {
+    console.log("Register Error", error.message);
+    res.send({ success: false, error: error.message });
+  }
+};
+
+
+module.exports.list = async (req, res) => {
+  try {
+
+    console.log("hello from register", req.body);
+
+    const users = await User.find().select("-password");
+    if(!users) return res.send({ success: false, errorId: 1 });
+
+
+    res.send({ success: true,users });
+  } catch (error) {
+    console.log("Register Error", error.message);
+    res.send({ success: false, error: error.message });
+  }
+};
+
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 
-router.post('/register', userController.register )
-router.post('/login')
+
+router.post('/register', userController.register)
+router.post('/login', userController.login)
+router.get('/list', userController.list)
+// router.post('/login')
 
 module.exports = router;
